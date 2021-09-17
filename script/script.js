39e9cf51fbffc83ebc351fc3bde8c108d73c444b
Samuel <59765994+samuelng87@users.noreply.github.com>
2021-09-17 18:44:45 +1000

    css/js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 let slideIndex = 0;
 showSlides();
 
-
+/*this is the function for mobile phone click on hamburger*/
 function myFunction() {
     let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -11,6 +11,7 @@ function myFunction() {
     }
   }
 
+/*this is a function for slide photo in index and partner and review  HTML*/
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -28,6 +29,26 @@ function showSlides() {
   setTimeout(showSlides, 10000); // Change image every 10 seconds
 }
 
-function contactClick() {
-  alert("Your request has been sent!");
+/*this is a function for contact details in contact HTML*/
+function alertClick() {
+    const text = 'sent'
+    document.getElementById("submit").innerHTML = text;
+    alert(' Notice: Details alert! \n Your contact details has been sent')
+
+}
+
+/*this is a function for appointment contact HTML*/
+function apptClick() {
+  let green;
+  let red;
+  if (confirm('Your appointment will be scheduled and we will contact you one day in advance\n\nWill you be okay with that?')) {
+    green = "Appointment Confirmed";
+    document.getElementById("green").innerHTML = green;
+    alert('Your appointment is set')
+  } else {
+    red = "Appointment Cancelled";
+    document.getElementById("red").innerHTML = red;
+  }
+  
+  
 }
